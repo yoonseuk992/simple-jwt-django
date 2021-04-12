050803a960d0812b6263745e08140d799ddbe4a4
John Yoon <sjy@sccoaching.com>
2021-04-12 23:43:01 +0900

    Remove unused import.

--- app/account/views.py
+++ app/account/views.py
@@ -1,12 +1,13 @@
 # Create your views here.
+import datetime
+
 from rest_framework import permissions
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
+from rest_framework_jwt.settings import api_settings
 from rest_framework_jwt.views import ObtainJSONWebToken
 
 from account.serializer import UserSerializer
-from rest_framework_jwt.settings import api_settings
-import datetime
 
 jwt_response_payload_handler = api_settings.JWT_RESPONSE_PAYLOAD_HANDLER
 
